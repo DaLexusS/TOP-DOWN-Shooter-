1aa4f6014a7e1a16d2e97bfa32285c80c050f257
DaLexusS <jenya.flieshman@gmail.com>
2024-04-21 15:01:28 +0300

    Update 1.2
    -Added health score number
    -Added Heart Icon

--- Assets/Scripts/MainGame/Attributes/Score.cs
+++ Assets/Scripts/MainGame/Attributes/Score.cs
@@ -3,9 +3,19 @@ using UnityEngine;
 public class Score : MonoBehaviour
 {
     [SerializeField] public int score;
+    [SerializeField] public int bestScore;
 
     public void AddScore(int amount)
     {
         score += amount;
+        if (bestScore < score)
+        {
+            bestScore = score;
+        }
+    }
+
+    public void ResetScore()
+    {
+        score = 0;
     }
 }
--- Assets/Scripts/UI/HealthBarUI.cs
+++ Assets/Scripts/UI/HealthBarUI.cs
@@ -1,12 +1,15 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class HealthBarUI : MonoBehaviour
 {
     [SerializeField] private Image _healthBarUi;
-
+    [SerializeField] private TextMeshProUGUI _healthScore;
+    private string maxHealthText = "/100";
     public void UpdateHealthBar(Health healthController)
     {
+        _healthScore.text = (healthController.GetHealthPercentage() * 100) + maxHealthText;
         _healthBarUi.fillAmount = healthController.GetHealthPercentage();
     }
 }
